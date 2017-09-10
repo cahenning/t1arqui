0914d43758214fee88e27303236212edb023c74c
cahenning <cahenning@uc.cl>
2017-09-10 16:00:51 -0300

    Added database connection

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  /* Register new request */
+  var Request = mongoose.model('Request');
+  var currentRequest = new Request({
+    ipAddress: req.ip,
+    timestamp: Date.now()
+  });
+  currentRequest.save(function (err) {
+    if (err) return handleError(err);
+  });
+
+  /* Show last 10 requests */
+  Request.
+    find({}).
+    sort('-timestamp').
+    limit(10).
+    select('ipAddress timestamp').
+    exec(function (err, requests) {
+      if (err) return handleError(err);
+      else {
+        res.render('index', { title: 'T1', 'requests': requests });
+      }
+    });
 });
 
 module.exports = router;
